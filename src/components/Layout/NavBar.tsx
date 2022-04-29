e89f600477ac6ba2c36474076526aba3174e30b8
QUAN Ngo Huu <quan.nh.25595@gmail.com>
2022-04-29 17:32:29 +0700

    add NavBar

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,13 +1,21 @@
 import {
   Alignment,
-  Button,
   Classes,
+  Icon,
   Navbar,
   NavbarDivider,
   NavbarGroup,
   NavbarHeading,
 } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+import styles from './NavBar.module.scss';
+
+const getNavLinkClassNames = ({ isActive }: { isActive: boolean }) => {
+  return classNames(Classes.TEXT_MUTED, styles.navLink, { [styles.active]: isActive });
+};
 
 const NavBar = () => {
   return (
@@ -15,8 +23,14 @@ const NavBar = () => {
       <NavbarGroup align={Alignment.LEFT}>
         <NavbarHeading>MindX Teaching Manager</NavbarHeading>
         <NavbarDivider />
-        <Button className={Classes.MINIMAL} icon={IconNames.HOME} text="Home" />
-        <Button className={Classes.MINIMAL} icon={IconNames.Document} text="Files" />
+        <NavLink to="/" className={getNavLinkClassNames}>
+          <Icon icon={IconNames.HOME} />
+          <span>Home</span>
+        </NavLink>
+        <NavLink to="/teacher" className={getNavLinkClassNames}>
+          <Icon icon={IconNames.CLIPBOARD} />
+          <span>Teachers</span>
+        </NavLink>
       </NavbarGroup>
     </Navbar>
   );
