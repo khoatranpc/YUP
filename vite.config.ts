61a5b42ea262057e49f9d7dcae227deaba586494
QUAN Ngo Huu <quan.nh.25595@gmail.com>
2022-04-29 17:30:37 +0700

    Load env & api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [tsconfigPaths(), react()],
+    server: {
+      port: +process.env.VITE_PORT,
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BACKEND_API_ENDPOINT,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  });
+};
