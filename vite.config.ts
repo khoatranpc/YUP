066b68b425731a1fce99655206e4c01a7f48c57d
QUAN Ngo Huu <quan.nh.25595@gmail.com>
2022-05-20 18:47:30 +0700

    change vite config for windows compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [tsconfigPaths(), react()],
     server: {
-      port: +process.env.VITE_PORT,
+      port: +env.VITE_PORT,
       proxy: {
         '/api': {
-          target: process.env.VITE_BACKEND_API_ENDPOINT,
+          target: env.VITE_BACKEND_API_ENDPOINT,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
