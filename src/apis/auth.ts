c87cbe1895dcdbec05cc6f363ebee22812345d9b
Trần Đăng Khoa <khoatranpc603@gmail.com>
2022-06-14 23:12:22 +0700

    add  typesafe auth login

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 import { AuthLogin } from 'types/user';
 export { login };
+interface response {
+  data: {
+    accessToken: string;
+    user: {
+      email: string;
+      id: string;
+      isVerified: boolean;
+      role: string[];
+    };
+  };
+}
 const login = (auth: AuthLogin) => {
   return axios
     .post('/api/user/login', auth)
-    .then((result) => {
+    .then((result: response) => {
       return result.data;
     })
     .catch((err) => {
